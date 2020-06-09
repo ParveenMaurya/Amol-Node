c65a019779158c3f56878a0224c844cbc3ff1a3d
ParveenMaurya <66665817+ParveenMaurya@users.noreply.github.com>
2020-06-09 19:48:54 +0530

    route
    route update

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,8 @@
 
 module.exports = function(app) {
 	
-	const controller = require('../controllers/controller_mongo');
+	// const controller = require('../controllers/controller_mongo');
+	const controller = require('../controllers/controller_mongo_modified');
 	
 	app.post("/sevenUpDownTopup",controller.sevenUpDownTopup);
 }
